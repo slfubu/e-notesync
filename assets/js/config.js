8a44778b0b569dd109ce6540e10399caad14b7e8
slfubu <thapanutputtathakorn@gmail.com>
2026-08-26 15:21:42 +0700

    Delete assets/js directory

--- assets/js/config.js
+++
@@ -1,5 +0,0 @@
-window.APP_CONFIG = Object.freeze({
-  APPS_SCRIPT_API_URL: "https://script.google.com/macros/s/AKfycbwYqfHYTQuuU6O4mU-Y1BJgFxRRIEjrtzZ2dRqFXYtm0FdtRH9gEr_Widn69ApUFnwB/exec",
-  REQUEST_TIMEOUT_MS: 45000,
-  SESSION_KEY: "ubuMemoSession"
-});
